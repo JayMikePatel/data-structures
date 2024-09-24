012c4d5d9bb91930f1778c44c1d3d51acd806c6b
JayMikePatel <151184736+JayMikePatel@users.noreply.github.com>
2024-09-24 16:55:44 -0400

    Create FibonacciRecursive.java

---
+++ FibonacciRecursive.java
@@ -0,0 +1,12 @@
+public class FibonacciRecursive {
+	static long Fibonacci(long n) {
+		if (n <= 1)
+			return n;
+		else
+			return Fibonacci(n-1) + Fibonacci(n-2);
+	}
+	public static void main(String[] args) {
+		System.out.println("The " + 5 + "th Fibonacci number is " + Fibonacci(10));
+		System.out.println("The " + 10 + "th Fibonacci number is " + Fibonacci(10));
+		System.out.println("The " + 15 + "th Fibonacci number is " + Fibonacci(15));
+	}
