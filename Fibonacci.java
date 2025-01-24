2b641b87b074421a84988954a06efb9d424eb70f
Jay Patel <151184736+JayMikePatel@users.noreply.github.com>
2025-01-24 12:20:20 -0500

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int nth = 5;
+        int first = 0;
+        int second = 1;
+        int temp = 0;
+        int i;
+        for(i = 0; i < nth; i++) {
+            temp = first;
+            first = second;
+            second += temp;
+        }
+        System.out.println(temp);
+    }
+}
