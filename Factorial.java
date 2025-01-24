e065e1052dd5cadb4868ad21dfb64c640d6fa28e
Jay Patel <151184736+JayMikePatel@users.noreply.github.com>
2025-01-24 12:19:51 -0500

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 10;
+        int total = 1;
+        int i;
+        for(i = 1; i <= n; i++) {
+            total *= i;
+        }
+        System.out.println(total);
+    }
+}
